35f53a2f66fe96741b7605acf88bd2d78a042634
Thomas Burette <burettethomas@gmail.com>
2014-04-11 18:57:53 +0200

    fleshing out structure

--- log-progress.py
+++ log-progress.py
@@ -1,7 +1,36 @@
+import argparse
 import datetime
+import time
 
-start = datetime.datetime.now()
 
-while True:
-    time.sleep(15)
-    print datetime.datetime.now() - start
+def delta_since(start_delta):
+    delta =  datetime.datetime.now() - start_delta
+    #second level precision
+    delta = delta - datetime.timedelta(microseconds=delta.microseconds)
+    return delta
+
+def run(file):
+    start = datetime.datetime.now()
+    while True:
+        user_string = raw_input()
+        delta = delta_since(start)
+        output = "{0}> {1}".format(delta, user_string)
+        print output
+        print >>file, output
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Gauge your productivity by writing down when you " + 
+        "accomplished a new task. Review progress later.")
+    parser.add_argument(
+        'file',
+        type=argparse.FileType('a+'),
+        help="File to write to")
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    global args
+    args =  parse_args()
+    try:
+        run(args.file)
+    except:
