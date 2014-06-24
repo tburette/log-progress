5dd974e9fb7677de6144d2db1b2c2f01bf24d229
Thomas Burette <burettethomas@gmail.com>
2014-06-24 17:31:15 +0200

    better C-c, C-d quit handling, better display, better date handling

--- log-progress.py
+++ log-progress.py
@@ -5,21 +5,30 @@ import time
 import re
 import sys
 
+def output_app_message(file, delta, message, time):
+    print >>file, "{0}>>{1} @ {2}".format(
+        delta, 
+        message,
+        time)
 
-def delta_since(start_delta):
-    delta =  datetime.datetime.now() - start_delta
+def delta_since(start_date):        
+    delta =  datetime.datetime.now() - start_date
     #second level precision
     delta = delta - datetime.timedelta(microseconds=delta.microseconds)
     return delta
 
-def run(file):
-    start = datetime.datetime.now()
+def run(start_date, file):
     while True:
-        user_string = raw_input()
-        delta = delta_since(start)
-        output = "{0}> {1}".format(delta, user_string)
+        #delta_since must be after raw_input 
+        #to compute date at which text was input
+        input = raw_input(">")
+        output = "{1}> {0}".format(
+            input,
+            delta_since(start_date))
         print output
-        print >>file, output
+        if input.strip().lower() != "time":
+            print >>file, output
+        file.flush()
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -33,9 +42,43 @@ def parse_args():
         help="File to write to")
     return parser.parse_args()
 
+def read_last_delta(file_name):
+    #quick and dirty
+    lines = open(file_name, 'r').readlines()
+    lines.reverse()
+    for line in lines:
+        #should use strptime
+        match = re.match(r'^(\d*):(\d{1,2}):(\d{1,2})>', line)
+        if(match):
+            ints = [int(x) for x in match.groups()]
+            seconds = ints[0]*60*60 + ints[1]*60 + ints[2]
+            return datetime.timedelta(seconds=seconds)
+    return None
+
+def compute_start_date(file_name):
+    last_delta = read_last_delta(file_name)
+    if last_delta:
+        return datetime.datetime.now() - last_delta
+    else:
+        return datetime.datetime.now()
+
 if __name__ == '__main__':
     global args
     args =  parse_args()
+    start_date = compute_start_date(args.file.name)
     try:
-        run(args.file)
-    except:
+        output_app_message(
+            args.file, 
+            delta_since(start_date),
+            "Start", 
+            datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+        
+        run(start_date, args.file)
+    finally:
+        #BUG: try to write even if problem with the output file, 
+        #parsing or a fatal error
+        output_app_message(
+            args.file, 
+            delta_since(start_date),
+            "Stop", 
+            datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
